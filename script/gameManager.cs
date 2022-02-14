7be8491f035630c25c2edaf3b045aa29b389193c
yoshihiro <jossism1@icloud.com>
2022-02-15 00:06:46 +0900

    clear transition

--- script/gameManager.cs
+++ script/gameManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject clearUI;
+
     void Start()
     {
-        
+        clearUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject[] trees = GameObject.FindGameObjectsWithTag("tree");
+        Debug.Log(trees.Length);
+        if(trees.Length == 0) {
+            clear();
+
+        }
         
+    }
+    void clear() {
+        clearUI.SetActive(true);
     }
 }
