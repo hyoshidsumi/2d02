54c3ade0277f79d07e8f1a9239471568ce85f9d4
yoshizumi <yoshizumi@linkstaff.co.jp>
2022-03-04 09:39:25 +0900

    3/4 10:00

--- treant/treantController.cs
+++ treant/treantController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class treantController : MonoBehaviour
 {
-    public int health;
+    public int health_max;
     public float speed;
     public string ma,ms,md,mw;
     public string mhold, mdhold, mwhold, mahold;
@@ -19,12 +20,14 @@ public class treantController : MonoBehaviour
     string motionName = "";
     string lastMotion = "";
 //    string fireName = "";
-    bool isFire = false;
+    bool isFire = false ,isMove=true;
     bool isLeft;
+    int health;
     int r;
     float time;
     pController p;
     public GameObject arrowPrefab;
+    public Slider hSlider;
 
     void Start()
     {
@@ -32,6 +35,7 @@ public class treantController : MonoBehaviour
         motion = this.GetComponent<Animator>();
         motionName = mhold;
         lastMotion = mhold;
+        health = health_max;
     }
 
     void Update()
@@ -58,90 +62,128 @@ public class treantController : MonoBehaviour
             barrow.AddForce(b, ForceMode2D.Impulse);        
             Invoke("stopFire",0.4f);
 
+            isMove = false;
         }
 
-//        Debug.Log(dist);
-
         //move
-        time += Time.deltaTime;
-        if(time > 2.0f) {
-            time = 0;
+        if (isMove)
+        {
 
-            r=UnityEngine.Random.Range(0,4);//0,1,2,3  
-            switch(r) {
-                case 0: ix=1; iy=0; break;
-                case 1: ix=-1; iy=0; break;
-                case 2: ix=0; iy=1; break;
-                case 3: ix=0; iy=-1; break;
-                default: break;
-            }        
-           
-            switch(iy) {
-                case 1: 
-                    motionName = mw;
-                    switch(ix) {
-                        case 1: phi = 45; break;
-                        case -1: phi = -45; break;
-                        default: phi = 90; break;
-                    }
-                    break;
-                case -1:
-                    motionName = ms;
-                    switch(ix) {
-                        case 1: phi = 135; break;
-                        case -1: phi = -135; break;
-                        default: phi = 180; break;
-                    }
-                    break;
-                default:
-                switch(ix) {
-                    case 1: 
-                        motionName = md;
-                        transform.localScale = new Vector3(11,11,1);       
-                        phi = 0;
-                        isLeft = false;
+            time += Time.deltaTime;
+            if (time > 2.0f)
+            {
+                time = 0;
+                r = UnityEngine.Random.Range(0, 4);//0,1,2,3  
+                switch (r)
+                {
+                    case 0: ix = 1; iy = 0; break;
+                    case 1: ix = -1; iy = 0; break;
+                    case 2: ix = 0; iy = 1; break;
+                    case 3: ix = 0; iy = -1; break;
+                    default: break;
+                }
+
+                switch (iy)
+                {
+                    case 1:
+                        motionName = mw;
+                        switch (ix)
+                        {
+                            case 1: phi = 45; break;
+                            case -1: phi = -45; break;
+                            default: phi = 90; break;
+                        }
                         break;
                     case -1:
-                        motionName = md;
-                        transform.localScale = new Vector3(-11,11,1);
-                        phi = -90;
-                        isLeft = true;
+                        motionName = ms;
+                        switch (ix)
+                        {
+                            case 1: phi = 135; break;
+                            case -1: phi = -135; break;
+                            default: phi = 180; break;
+                        }
                         break;
                     default:
-                        if((lastMotion=="md")||(lastMotion=="mdhold")) {
-                            motionName = mdhold;
-                            if(isLeft == false) {
-                                phi = 90;
-                            } else {
-                                transform.localScale = new Vector3(-11,11,1);
+                        switch (ix)
+                        {
+                            case 1:
+                                motionName = md;
+                                transform.localScale = new Vector3(11, 11, 1);
+                                phi = 0;
+                                isLeft = false;
+                                break;
+                            case -1:
+                                motionName = md;
+                                transform.localScale = new Vector3(-11, 11, 1);
                                 phi = -90;
-                            }
-                        } else if((lastMotion=="mw")||(lastMotion=="mwhold")) {
-                            motionName = mwhold;
-                            phi = 0;
-                        } else {
-                            motionName = mhold;
-                            phi = 180;
+                                isLeft = true;
+                                break;
+                            default:
+                                if ((lastMotion == "md") || (lastMotion == "mdhold"))
+                                {
+                                    motionName = mdhold;
+                                    if (isLeft == false)
+                                    {
+                                        phi = 90;
+                                    }
+                                    else
+                                    {
+                                        transform.localScale = new Vector3(-11, 11, 1);
+                                        phi = -90;
+                                    }
+                                }
+                                else if ((lastMotion == "mw") || (lastMotion == "mwhold"))
+                                {
+                                    motionName = mwhold;
+                                    phi = 0;
+                                }
+                                else
+                                {
+                                    motionName = mhold;
+                                    phi = 180;
+                                }
+                                break;
                         }
                         break;
                 }
-                break;
-            }
-            
-            if(motionName != lastMotion) {
-                motion.Play(motionName);
-                lastMotion = motionName;
-            }
-        }
 
+                if (motionName != lastMotion)
+                {
+                    motion.Play(motionName);
+                    lastMotion = motionName;
+                }
+            }
+        } else
+        {
+
+        }
     }
 
     public void stopFire() {
         isFire = false;
+        isMove = true;
     }
 
     private void FixedUpdate()
     {
         rbody.velocity = new Vector2(ix*speed, iy*speed);
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "arrow1")
+        {
+            health = health - 4 ;
+        }
+
+        hSlider.value = (float)health / (float)health_max;
+        Debug.Log(health + " " + health_max);
+
+
+        if (health < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
